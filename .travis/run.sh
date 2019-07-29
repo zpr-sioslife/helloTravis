55ea23424fc13a86c598bd540586bec9db54640b
José Rocha <joserocha@sioslife.com>
2019-07-29 13:35:59 +0100

    ios

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,24 +1,9 @@
 #!/bin/sh
 set -e
-# if [ "$LANE" = "node" ]; then
-#     yarn install
-#     npm install -g react-native-cli
-#     make install
-# else
-#     if [ "$TRAVIS_SECURE_ENV_VARS" != "true" ] ; then
-#         echo "SKIPPING device tests since not secure env"
-#         exit 0
-#     fi
-
-    
-#     npm run release
-
-#     # if [ "$LANE" = "ios" ]; then
-#     #     make test
-#     # else
-#     #     make test-android
-#     # fi
-# fi
-
 npm install
-npm run release
+
+if [ "$LANE" = "ios" ]; then
+  npm run release:ios 
+elif [ "$LANE" = "android" ]; then
+  npm run release:android
+fi
